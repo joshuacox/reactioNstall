d5d5ac7f9afedb920cea8f6b02d272a6e3ab4e77
Josh Cox <josh@webhosting.coop>
2017-12-22 11:10:06 -0600

    debug

--- .ci/envtest.sh
+++ .ci/envtest.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -l
 printenv
 PATH=~/.meteor:$PATH
-echo $PATH
+which meteor
+npm -v
+which npm
+echo -n 'node version: '
+node -v
+which node
+nvm ls
+nvm use --lts lts/carbon
+echo PATH is $PATH
