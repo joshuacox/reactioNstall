a358907d6aac4b78f5f79d5a6a8242a94ce67355
Josh Cox <josh@webhosting.coop>
2017-12-22 01:18:20 -0600

    Check for .git dir

--- .ci/test.sh
+++ .ci/test.sh
@@ -5,8 +5,10 @@ cd $HOME
 ls -lh .meteor
 which reaction
 which meteor
-if [ ! -d "$HOME/reaction" ]; then
+if [ ! -d "$HOME/reaction/.git" ]; then
   reaction init
+else
+  ls -lh $HOME/reaction
 fi
 cd $HOME/reaction
 reaction --version
