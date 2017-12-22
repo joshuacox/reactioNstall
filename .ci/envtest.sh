8bd29b36c58d725d4add1aa56c66c98894688d76
Josh Cox <josh@webhosting.coop>
2017-12-22 11:16:52 -0600

    doubledown

--- .ci/envtest.sh
+++ .ci/envtest.sh
@@ -2,6 +2,7 @@
 printenv
 PATH=~/.meteor:$PATH
 which meteor
+echo -n 'npm version: '
 npm -v
 which npm
 echo -n 'node version: '
@@ -10,3 +11,14 @@ which node
 nvm ls
 nvm use --lts lts/carbon
 echo PATH is $PATH
+echo doubledown
+export NVM_DIR="/home/thoth/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm ls
+nvm use --lts lts/carbon
+echo -n 'npm version: '
+npm -v
+which npm
+echo -n 'node version: '
+node -v
+which node
