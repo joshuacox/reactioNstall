b333171841a1d56f9c274d1c2c0c316e1108b3ae
Josh Cox <josh@webhosting.coop>
2017-12-22 11:20:22 -0600

    What is in the nvm dir?

--- .ci/envtest.sh
+++ .ci/envtest.sh
@@ -11,8 +11,9 @@ which node
 nvm ls
 nvm use --lts lts/carbon
 echo PATH is $PATH
+ls -lh "$HOME/.nvm"
 echo doubledown
-export NVM_DIR="/home/thoth/.nvm"
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm ls
 nvm use --lts lts/carbon
